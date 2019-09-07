50e4f3f80098c4ef35045253f9ed1fc07f768fe5
lmatteis <lmatteis@gmail.com>
2019-09-07 21:26:47 +0200

    forEach

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,11 @@
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(fn, scope) {
+        for (var i = 0, len = this.length; i < len; ++i) {
+            fn.call(scope, this[i], i, this)
+        }
+    }
+}
+
 window.onload = function() {
     var root = null
     var useHash = true // Defaults to: false
