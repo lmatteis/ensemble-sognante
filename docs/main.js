e382ab0aae111e2f449e4892616175c6dd6e6891
lmatteis <lmatteis@gmail.com>
2019-09-07 22:30:55 +0200

    sdfjisdf

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
+if (!document.querySelectorAll) {
+    document.querySelectorAll = function(selectors) {
+        var style = document.createElement('style'),
+            elements = [],
+            element;
+        document.documentElement.firstChild.appendChild(style);
+        document._qsa = [];
+
+        style.styleSheet.cssText =
+            selectors +
+            '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+        window.scrollBy(0, 0);
+        style.parentNode.removeChild(style);
+
+        while (document._qsa.length) {
+            element = document._qsa.shift();
+            element.style.removeAttribute('x-qsa');
+            elements.push(element);
+        }
+        document._qsa = null;
+        return elements;
+    };
+}
+
+if (!document.querySelector) {
+    document.querySelector = function(selectors) {
+        var elements = document.querySelectorAll(selectors);
+        return elements.length ? elements[0] : null;
+    };
+}
+
 if (!Array.prototype.forEach) {
     Array.prototype.forEach = function(fn, scope) {
         for (var i = 0, len = this.length; i < len; ++i) {
@@ -26,7 +57,7 @@ window.onload = function() {
     var converter = new showdown.Converter();
     var home = document.querySelector('#PAGES_CONTAINER');
     var main = document.querySelector('#main');
-    console.log(home, main);
+    alert(home);
 
     router
         .on({
