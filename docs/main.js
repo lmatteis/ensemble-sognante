56a61c9e25eb32571e18f77cb57dc3bf2bf96531
Luca Matteis <luca.matteis@dazn.com>
2019-09-25 16:40:02 +0200

    Linkify

--- docs/main.js
+++ docs/main.js
@@ -41,6 +41,33 @@ if (!Array.prototype.forEach) {
 
 var pages = {};
 
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(
+        replacePattern1,
+        '<a href="$1" target="_blank">$1</a>'
+    );
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(
+        replacePattern2,
+        '$1<a href="http://$2" target="_blank">$2</a>'
+    );
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(
+        replacePattern3,
+        '<a href="mailto:$1">$1</a>'
+    );
+
+    return replacedText;
+}
+
 window.onload = function() {
     var root = null;
     var useHash = true; // Defaults to: false
@@ -102,7 +129,7 @@ function setContent(page) {
     home.style.display = 'none';
     pageDiv.style.display = 'block';
     navHighlight(page);
-    pageDiv.innerHTML = converter.makeHtml(content);
+    pageDiv.innerHTML = converter.makeHtml(linkify(content));
 }
 
 function navHighlight(page) {
